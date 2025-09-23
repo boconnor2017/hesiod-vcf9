3ff3df5846fb2916d012303991c2a20c1ca16fd1
[Brendan OConnor] <[boconnor@vmware.com]>
2025-09-23 09:34:24 -0400

    bugfix v14

--- lib/esxi_on_esxi.py
+++ lib/esxi_on_esxi.py
@@ -135,6 +135,7 @@ def pcli_prep_hosts_for_vcf(env_json_py, mgt, vi):
         while i < len(env_json_py["nested_esxi_servers"]["management_host_specs"]):
             print("    Prepping ESXi Host"+ str(i)+": "+env_json_py["nested_esxi_servers"]["management_host_specs"][i]["name_of_vm"])
             # Create list of hosts
+            # FIX THIS: THIS PART IS BROKEN - HOSTLIST RETURNS POOL.NTP.ORG
             hostlist = hostlist+env_json_py["nested_esxi_servers"]["management_host_specs"][i]["nested_esxi_ip_address"]+"\", "
             # Lookup network
             search_and_replace_in_file("ID:SIV-006", env_json_py["physical_server"][env_json_py["nested_esxi_servers"]["management_host_specs"][i]["deploy_to_physical_host"]]["deploy_vms_to_this_network"], prep_esxi_hosts_for_vcf_script_name)
